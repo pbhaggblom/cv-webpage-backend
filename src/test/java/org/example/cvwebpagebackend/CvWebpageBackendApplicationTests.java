2c9e92ba18c688726a330b05be1002bf2d9541c0
Pontus Häggblom <pbhaggblom@gmail.com>
2025-09-16 13:54:47 +0200

    Initial commit

---
+++ src/test/java/org/example/cvwebpagebackend/CvWebpageBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cvwebpagebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvWebpageBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
