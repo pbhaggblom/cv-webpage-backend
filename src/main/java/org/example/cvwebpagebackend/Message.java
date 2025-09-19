1ae898e404f588c4701a324a4cd7541d8a216eb8
Pontus Häggblom <pbhaggblom@gmail.com>
2025-09-19 16:12:12 +0200

    Adds functionality for sending an email notification when a message is submitted.

--- src/main/java/org/example/cvwebpagebackend/Message.java
+++ src/main/java/org/example/cvwebpagebackend/Message.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Message {
 
     @Id
@@ -20,6 +24,4 @@ public class Message {
         this.email = email;
         this.message = message;
     }
-
-    public Message() {}
 }
